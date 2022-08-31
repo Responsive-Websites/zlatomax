a78c46edef398ab1ff4c645e2ba38480f16bd925
Danila-hub <kontaigabox@mail.ru>
2022-08-31 18:36:06 +0300

    Added adaptive styles for header

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,13 @@
 
 // burger
 
-// let header_menu = document.querySelector('.menu__body');
-// let burger_icon = document.querySelector('.icon-menu');
-// burger_icon.addEventListener('click', function (e) {
-//   header_menu.classList.toggle('_active');
-//   burger_icon.classList.toggle('_active');
-//   document.body.classList.toggle('_lock');
-// });
+let header_menu = document.querySelector('.menu__body');
+let burger_icon = document.querySelector('.icon-menu');
+burger_icon.addEventListener('click', function (e) {
+  header_menu.classList.toggle('_active');
+  burger_icon.classList.toggle('_active');
+  document.body.classList.toggle('_lock');
+});
 
 // =========================================================
 
@@ -66,163 +66,163 @@
 // Dynamic Adaptive
 // data-da=".menu__body,767,1"
 
-// function DynamicAdapt(type) {
-//     this.type = type;
-//   }
+function DynamicAdapt(type) {
+  this.type = type;
+}
 
-//   DynamicAdapt.prototype.init = function () {
-//     const _this = this;
-//     // массив объектов
-//     this.оbjects = [];
-//     this.daClassname = '_dynamic_adapt_';
-//     // массив DOM-элементов
-//     this.nodes = document.querySelectorAll('[data-da]');
+DynamicAdapt.prototype.init = function () {
+  const _this = this;
+  // массив объектов
+  this.оbjects = [];
+  this.daClassname = '_dynamic_adapt_';
+  // массив DOM-элементов
+  this.nodes = document.querySelectorAll('[data-da]');
 
-//     // наполнение оbjects объктами
-//     for (let i = 0; i < this.nodes.length; i++) {
-//       const node = this.nodes[i];
-//       const data = node.dataset.da.trim();
-//       const dataArray = data.split(',');
-//       const оbject = {};
-//       оbject.element = node;
-//       оbject.parent = node.parentNode;
-//       оbject.destination = document.querySelector(dataArray[0].trim());
-//       оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
-//       оbject.place = dataArray[2] ? dataArray[2].trim() : 'last';
-//       оbject.index = this.indexInParent(оbject.parent, оbject.element);
-//       this.оbjects.push(оbject);
-//     }
+  // наполнение оbjects объктами
+  for (let i = 0; i < this.nodes.length; i++) {
+    const node = this.nodes[i];
+    const data = node.dataset.da.trim();
+    const dataArray = data.split(',');
+    const оbject = {};
+    оbject.element = node;
+    оbject.parent = node.parentNode;
+    оbject.destination = document.querySelector(dataArray[0].trim());
+    оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : '767';
+    оbject.place = dataArray[2] ? dataArray[2].trim() : 'last';
+    оbject.index = this.indexInParent(оbject.parent, оbject.element);
+    this.оbjects.push(оbject);
+  }
 
-//     this.arraySort(this.оbjects);
+  this.arraySort(this.оbjects);
 
-//     // массив уникальных медиа-запросов
-//     this.mediaQueries = Array.prototype.map.call(
-//       this.оbjects,
-//       function (item) {
-//         return '(' + this.type + '-width: ' + item.breakpoint + 'px),' + item.breakpoint;
-//       },
-//       this
-//     );
-//     this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
-//       return Array.prototype.indexOf.call(self, item) === index;
-//     });
+  // массив уникальных медиа-запросов
+  this.mediaQueries = Array.prototype.map.call(
+    this.оbjects,
+    function (item) {
+      return '(' + this.type + '-width: ' + item.breakpoint + 'px),' + item.breakpoint;
+    },
+    this
+  );
+  this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+    return Array.prototype.indexOf.call(self, item) === index;
+  });
 
-//     // навешивание слушателя на медиа-запрос
-//     // и вызов обработчика при первом запуске
-//     for (let i = 0; i < this.mediaQueries.length; i++) {
-//       const media = this.mediaQueries[i];
-//       const mediaSplit = String.prototype.split.call(media, ',');
-//       const matchMedia = window.matchMedia(mediaSplit[0]);
-//       const mediaBreakpoint = mediaSplit[1];
+  // навешивание слушателя на медиа-запрос
+  // и вызов обработчика при первом запуске
+  for (let i = 0; i < this.mediaQueries.length; i++) {
+    const media = this.mediaQueries[i];
+    const mediaSplit = String.prototype.split.call(media, ',');
+    const matchMedia = window.matchMedia(mediaSplit[0]);
+    const mediaBreakpoint = mediaSplit[1];
 
-//       // массив объектов с подходящим брейкпоинтом
-//       const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
-//         return item.breakpoint === mediaBreakpoint;
-//       });
-//       matchMedia.addListener(function () {
-//         _this.mediaHandler(matchMedia, оbjectsFilter);
-//       });
-//       this.mediaHandler(matchMedia, оbjectsFilter);
-//     }
-//   };
+    // массив объектов с подходящим брейкпоинтом
+    const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+      return item.breakpoint === mediaBreakpoint;
+    });
+    matchMedia.addListener(function () {
+      _this.mediaHandler(matchMedia, оbjectsFilter);
+    });
+    this.mediaHandler(matchMedia, оbjectsFilter);
+  }
+};
 
-//   DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
-//     if (matchMedia.matches) {
-//       for (let i = 0; i < оbjects.length; i++) {
-//         const оbject = оbjects[i];
-//         оbject.index = this.indexInParent(оbject.parent, оbject.element);
-//         this.moveTo(оbject.place, оbject.element, оbject.destination);
-//       }
-//     } else {
-//       for (let i = 0; i < оbjects.length; i++) {
-//         const оbject = оbjects[i];
-//         if (оbject.element.classList.contains(this.daClassname)) {
-//           this.moveBack(оbject.parent, оbject.element, оbject.index);
-//         }
-//       }
-//     }
-//   };
+DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+  if (matchMedia.matches) {
+    for (let i = 0; i < оbjects.length; i++) {
+      const оbject = оbjects[i];
+      оbject.index = this.indexInParent(оbject.parent, оbject.element);
+      this.moveTo(оbject.place, оbject.element, оbject.destination);
+    }
+  } else {
+    for (let i = 0; i < оbjects.length; i++) {
+      const оbject = оbjects[i];
+      if (оbject.element.classList.contains(this.daClassname)) {
+        this.moveBack(оbject.parent, оbject.element, оbject.index);
+      }
+    }
+  }
+};
 
-//   // Функция перемещения
-//   DynamicAdapt.prototype.moveTo = function (place, element, destination) {
-//     element.classList.add(this.daClassname);
-//     if (place === 'last' || place >= destination.children.length) {
-//       destination.insertAdjacentElement('beforeend', element);
-//       return;
-//     }
-//     if (place === 'first') {
-//       destination.insertAdjacentElement('afterbegin', element);
-//       return;
-//     }
-//     destination.children[place].insertAdjacentElement('beforebegin', element);
-//   };
+// Функция перемещения
+DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+  element.classList.add(this.daClassname);
+  if (place === 'last' || place >= destination.children.length) {
+    destination.insertAdjacentElement('beforeend', element);
+    return;
+  }
+  if (place === 'first') {
+    destination.insertAdjacentElement('afterbegin', element);
+    return;
+  }
+  destination.children[place].insertAdjacentElement('beforebegin', element);
+};
 
-//   // Функция возврата
-//   DynamicAdapt.prototype.moveBack = function (parent, element, index) {
-//     element.classList.remove(this.daClassname);
-//     if (parent.children[index] !== undefined) {
-//       parent.children[index].insertAdjacentElement('beforebegin', element);
-//     } else {
-//       parent.insertAdjacentElement('beforeend', element);
-//     }
-//   };
+// Функция возврата
+DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+  element.classList.remove(this.daClassname);
+  if (parent.children[index] !== undefined) {
+    parent.children[index].insertAdjacentElement('beforebegin', element);
+  } else {
+    parent.insertAdjacentElement('beforeend', element);
+  }
+};
 
-//   // Функция получения индекса внутри родителя
-//   DynamicAdapt.prototype.indexInParent = function (parent, element) {
-//     const array = Array.prototype.slice.call(parent.children);
-//     return Array.prototype.indexOf.call(array, element);
-//   };
+// Функция получения индекса внутри родителя
+DynamicAdapt.prototype.indexInParent = function (parent, element) {
+  const array = Array.prototype.slice.call(parent.children);
+  return Array.prototype.indexOf.call(array, element);
+};
 
-//   // Функция сортировки массива по breakpoint и place
-//   // по возрастанию для this.type = min
-//   // по убыванию для this.type = max
-//   DynamicAdapt.prototype.arraySort = function (arr) {
-//     if (this.type === 'min') {
-//       Array.prototype.sort.call(arr, function (a, b) {
-//         if (a.breakpoint === b.breakpoint) {
-//           if (a.place === b.place) {
-//             return 0;
-//           }
+// Функция сортировки массива по breakpoint и place
+// по возрастанию для this.type = min
+// по убыванию для this.type = max
+DynamicAdapt.prototype.arraySort = function (arr) {
+  if (this.type === 'min') {
+    Array.prototype.sort.call(arr, function (a, b) {
+      if (a.breakpoint === b.breakpoint) {
+        if (a.place === b.place) {
+          return 0;
+        }
 
-//           if (a.place === 'first' || b.place === 'last') {
-//             return -1;
-//           }
+        if (a.place === 'first' || b.place === 'last') {
+          return -1;
+        }
 
-//           if (a.place === 'last' || b.place === 'first') {
-//             return 1;
-//           }
+        if (a.place === 'last' || b.place === 'first') {
+          return 1;
+        }
 
-//           return a.place - b.place;
-//         }
+        return a.place - b.place;
+      }
 
-//         return a.breakpoint - b.breakpoint;
-//       });
-//     } else {
-//       Array.prototype.sort.call(arr, function (a, b) {
-//         if (a.breakpoint === b.breakpoint) {
-//           if (a.place === b.place) {
-//             return 0;
-//           }
+      return a.breakpoint - b.breakpoint;
+    });
+  } else {
+    Array.prototype.sort.call(arr, function (a, b) {
+      if (a.breakpoint === b.breakpoint) {
+        if (a.place === b.place) {
+          return 0;
+        }
 
-//           if (a.place === 'first' || b.place === 'last') {
-//             return 1;
-//           }
+        if (a.place === 'first' || b.place === 'last') {
+          return 1;
+        }
 
-//           if (a.place === 'last' || b.place === 'first') {
-//             return -1;
-//           }
+        if (a.place === 'last' || b.place === 'first') {
+          return -1;
+        }
 
-//           return b.place - a.place;
-//         }
+        return b.place - a.place;
+      }
 
-//         return b.breakpoint - a.breakpoint;
-//       });
-//       return;
-//     }
-//   };
+      return b.breakpoint - a.breakpoint;
+    });
+    return;
+  }
+};
 
-//   const da = new DynamicAdapt('max');
-//   da.init();
+const da = new DynamicAdapt('max');
+da.init();
 
 // =========================================================
 
@@ -592,6 +592,13 @@ document.addEventListener('keydown', function (e) {
 
 //sub menu
 document.addEventListener('click', documentActions);
+const menuBlocks = document.querySelectorAll('.sub-menu-catalog__block');
+if (menuBlocks.length) {
+  menuBlocks.forEach((menuBlock) => {
+    const menuBlockItems = menuBlock.querySelectorAll('.sub-menu-catalog__category').length;
+    menuBlock.classList.add(`sub-menu-catalog__block_${menuBlockItems}`);
+  });
+}
 
 function documentActions(e) {
   const targetElement = e.target;
@@ -601,9 +608,14 @@ function documentActions(e) {
     const catalogMenu = document.querySelector('.catalog-header');
 
     if (subMenu) {
-      if (targetElement.classList.contains('._sub-menu-active')) {
-      } else {
+      const activeLink = document.querySelector('._sub-menu-active');
+      const activeBlock = document.querySelector('._sub-menu-open');
+      if (activeLink && activeLink !== targetElement) {
+        activeLink.classList.remove('_sub-menu-active');
+        activeBlock.classList.remove('_sub-menu-open');
       }
+      targetElement.classList.toggle('_sub-menu-active');
+      subMenu.classList.toggle('_sub-menu-open');
     } else {
       console.log('err');
     }
